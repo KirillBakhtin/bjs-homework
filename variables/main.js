7d1b6c80e681ee7e5024a89921bcbc0e07646302
Kirill Bakhtin <flash232323@gmail.com>
2019-03-23 00:16:33 +0600

    BJS Homework #1 added

---
+++ variables/main.js
@@ -0,0 +1,25 @@
+//ЗАДАНИЕ № 1
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+//ЗАДАНИЕ № 2
+
+let algebra = 5;
+let geography = 3;
+let physycs = 4;
+
+let averageMark = (algebra + geography + physycs) / 3;
+
+console.log(averageMark);
+
+//ЗАДАНИЕ № 3
+
+let name = 'Kirill Bakhtin';
+
+console.log(`Привет, мир! Меня зовут ${name}.`);
