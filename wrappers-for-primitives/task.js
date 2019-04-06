45f385e5e4251c55fa1c75ca312fc3c4b1b16a88
Kirill Bakhtin <flash232323@gmail.com>
2019-04-06 20:52:47 +0600

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,7 @@
+"use strict"
+
+//Задание № 2
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,10 +14,43 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    let endDate = new Date(date);
+    let endYear = endDate.getFullYear ();
+    let endMonth = endDate.getMonth ();
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
-}
+    let todaysDate = new Date ();
+    let todaysYear = todaysDate.getFullYear ();
+    let todaysMonth = todaysDate.getMonth ();
+
+    let contributionDate = ((endYear - todaysYear) * 12) + (endMonth - todaysMonth);
+ 
+    let argumentsArray = {
+        "percent": percent, 
+        "contribution": contribution, 
+        "amount": amount, 
+        "date": contributionDate
+    };
+    
+    for (let prop in argumentsArray) {
+        argumentsArray[prop] = parseInt (argumentsArray[prop]);
+        console.log(typeof (argumentsArray[prop]));
+        if (isNaN (argumentsArray[prop]) || argumentsArray[prop] < 0) {
+            return `Параметр ${prop} содержит неправильное значение ${argumentsArray[prop]}`;
+        } 
+    };
+
+    let paymentWithoutComtribution = amount - contribution;
+    let p = percent/100/12;
+     
+    let monthPayment = paymentWithoutComtribution * (p + p / (Math.pow ((1 + p), argumentsArray.date) - 1));
+
+    let  totalAmount = monthPayment * argumentsArray.date;
+    
+    console.log(totalAmount.toFixed (2));
+    return totalAmount.toFixed (2);
+};
+
+//Задание № 2
 
 function sayHello() {
     let name = window.personName.value;
@@ -23,6 +60,11 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if (Boolean(name) == false) {
+        console.log (`Привет, мир! Меня зовут Аноним`);
+        return `Привет, мир! Меня зовут Аноним`;
+    } else {
+        console.log (`Привет, мир! Меня зовут ${name}`);
+        return `Привет, мир! Меня зовут ${name}`
+    }
 }
